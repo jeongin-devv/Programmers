9472ddc38cff41116635bb3ba8a745a20a5f1b10
jeongin-devv <jeongin.devv@gmail.com>
2022-12-29 00:43:55 +0900

    update

---
+++ level-1/221208_문자열다루기기본.ts
@@ -0,0 +1,15 @@
+/* eslint-disabled */
+// const solution = (s : string) => {
+//     let answer = true;
+    
+//     if(s.indexOf('e') > 0 ) {
+//         if(isNaN(s.replaceAll('e','p'))) return false;
+//     }else{
+//         if(isNaN(s)) return false;
+//     }
+    
+//     if(s.length != 4 && s.length != 6) return false;
+    
+//     return answer;
+// }
+export default solution;
